bfe82304d6fb1e2e8c3d064d775f6b9fdff16bed
Fahad Ali <fahadali45621@gmail.com>
2026-04-04 10:46:20 +0500

    test case 2 complete

--- Project_2/main.py
+++ Project_2/main.py
@@ -3,9 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-import random
 import time
-import os
 
 driver = webdriver.Chrome()
 driver.get("https://automationexercise.com/")
